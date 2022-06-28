2e4120d244bfdfe15b3fbd44ea68cbd7c2f267f0
stylopm <apmurillo99@hotmail.com>
2022-06-27 20:21:00 -0500

    feat: Se agrega el ejercicio 01

---
+++ ejercicio01/version1/script.js
@@ -0,0 +1,12 @@
+//Ejercicio 1: Escribe un programa que pida dos números y escriba en la pantalla cuál es el mayor.
+console.log('Ejercicio 01');
+const numeroUno = parseInt(prompt('Digite el primer número'));
+const numeroDos = parseInt(prompt('Digite el segundo número'));
+// const resultado = document.getElementById('resultado');
+if (numeroUno === numeroDos) {
+  document.write('Los números son iguales '+numeroUno);
+} else if (numeroUno > numeroDos) {
+  document.write('El número '+numeroUno+ ' es mayor que '+numeroDos);
+} else {
+  document.write('El número '+numeroUno+ ' es menor que '+numeroDos);
+}
